fca154cb06b31a3b36b6afebb73cf2e063cb48e4
Jasmine Doyle <jasminejoy.doyle@gmail.com>
2023-02-08 16:42:14 +0000

    edited the code to define a function to decide which movie to watch

--- watch_next.py
+++ watch_next.py
@@ -11,24 +11,31 @@ with open("movies.txt", "r", encoding= "utf8") as file:
         movie = (line.split(" :")[1]).strip("\n")
         movies[title] = movie
 
-# description to compare to, convert to nlp form
-description_to_compare = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
-model_sentence = nlp(description_to_compare)
+# define a function which will tell the user which movie to watch
+# based on how similar it is to a description, which is the argument of the function
+def which_movie(description_to_compare):
+    model_sentence = nlp(description_to_compare)
 
-# create variable to store highest similarity score and corresponding movie title
-highest_similarity = 0
-movie_to_watch = None
+    # create variable to store highest similarity score and corresponding movie title
+    highest_similarity = 0
+    movie_to_watch = None
 
-# loop through the the dict
-# if similarity higher than current value, then set this as the highest similarity
-# and store the corresponding movie title
-for movie in movies:
-    similarity = nlp(movies[movie]).similarity(model_sentence)
-    if similarity > highest_similarity:
-        highest_similarity = similarity
-        movie_to_watch = movie
+    # loop through the the dict
+    # if similarity higher than current value, then set this as the highest similarity
+    # and store the corresponding movie title
+    for movie in movies:
+        similarity = nlp(movies[movie]).similarity(model_sentence)
+        if similarity > highest_similarity:
+            highest_similarity = similarity
+            movie_to_watch = movie
 
-# return which movie you should watch
-print(f"You should watch {movie_to_watch}.")
+    # return which movie you should watch
+    print(f"You should watch {movie_to_watch}.")
+
+
+
+# call the function with the following description to compare to
+description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+which_movie(description)
 
 # in this case, the user should watch Movie C
